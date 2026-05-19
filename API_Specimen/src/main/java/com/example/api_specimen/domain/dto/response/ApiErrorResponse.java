3c4ebbfe5082a4490445654034933b2be601c52b
JonaSeguReymundo <00043823@uca.edu.sv>
2026-05-18 21:28:43 -0600

    feat: add ApiErrorResponse schema for clean error feedback

---
+++ API_Specimen/src/main/java/com/example/api_specimen/domain/dto/response/ApiErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.api_specimen.domain.dto.response;
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResponse {
+    private Object message;
+    private int status;
+    private LocalDate time;
+    private String uri;
+}
