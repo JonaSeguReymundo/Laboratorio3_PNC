1ccd5c1f79ebf6565cd66ade8dadc032cc437d67
JonaSeguReymundo <00043823@uca.edu.sv>
2026-05-18 20:15:12 -0600

    chore: initialize project structure and dependencies

---
+++ API_Specimen/src/main/java/com/example/api_specimen/ApiSpecimenApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_specimen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpecimenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpecimenApplication.class, args);
+    }
+
+}
---
+++ API_Specimen/src/test/java/com/example/api_specimen/ApiSpecimenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_specimen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpecimenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
