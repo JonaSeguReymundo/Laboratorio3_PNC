63b6d53ff07df877a72310e230c955a2a8e88857
JonaSeguReymundo <00043823@uca.edu.sv>
2026-05-18 21:04:31 -0600

    feat: add UpdateSpecimenRequest for partial data modifications

---
+++ API_Specimen/src/main/java/com/example/api_specimen/domain/request/UpdateSpecimenRequest.java
@@ -0,0 +1,14 @@
+package com.example.api_specimen.domain.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateSpecimenRequest {
+    private String name;
+    private String region;
+    private Integer dangerLevel;
+    private Boolean isFriendly;
+}
