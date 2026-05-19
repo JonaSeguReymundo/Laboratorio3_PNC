9233b0e4e41293a6d7ceea5c683b5672a94ef123
JonaSeguReymundo <00043823@uca.edu.sv>
2026-05-18 20:54:57 -0600

    feat: add CreateSpecimenRequest with validation constraints

---
+++ API_Specimen/src/main/java/com/example/api_specimen/domain/request/CreateSpecimenRequest.java
@@ -0,0 +1,25 @@
+package com.example.api_specimen.domain.request;
+
+import com.example.api_specimen.common.validation.UniqueSpecimenName;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateSpecimenRequest {
+    @NotBlank(message = "The specimen name cannot be empty.")
+    @UniqueSpecimenName()
+    private String name;
+
+    @NotBlank(message = "The region of Hyrule must be specified.")
+    private String region;
+
+    @NotNull(message = "Danger level is required.")
+    private Integer dangerLevel;
+
+    @NotNull(message = "You must specify if the specimen is friendly.")
+    private Boolean isFriendly;
+}
